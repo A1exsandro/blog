af4bd6ce2d80490600b9a802b92117904c44833c
A1exsandro <alexsandro.acp@gmail.com>
2022-12-08 16:24:25 -0400

    changes in BlogCard

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,15 +1,33 @@
-import Link from "next/link"
-import styles from '../styles/BlogCard.module.css'
+import styles from "../styles/BlogCard.module.css";
+import moment from "moment";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function BlogCard({tilte, author, coverPhoto, datePublished, slug}){
-  return(
+function BlogPost({ title, author, coverPhoto, datePublished, slug }) {
+  return (
     <div className={styles.card}>
-      <Link href={"/posts/" + slug}>
+      <Link href={`/posts/${slug}`}>
         <div className={styles.imgContainer}>
-          <div>{author.name}</div>
-          <div>{coverPhoto.url}</div>
+          <Image layout="fill" src={coverPhoto.url} alt="" />
         </div>
       </Link>
+      <div className={styles.text}>
+        <h2>{title}</h2>
+        <div className={styles.details}>
+          <div className={styles.author}>
+            <img src={author.avatar.url} alt={author.name} />
+            <h3>{author.name}</h3>
+          </div>
+          <div className={styles.date}>
+            <h3>{moment(datePublished).format("MMMM d, YYYY")}</h3>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
+{
+  /* <div dangerouslySetInnerHTML={{ __html: content.html }}></div> */
+}
+export default BlogPost;
